e15a3fb895560cf225716ded28accb6e624f90ed
AbarnaRajan01 <abuabarna2@gmail.com>
2025-05-21 00:31:56 +0530

    Updated roadmap route

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
-})
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID || "Ov23lity0PR6HNcorjlf",
+      clientSecret: process.env.AUTH_GITHUB_SECRET || "d03d9d4c3babcf55a61f1c174da828e9b9b2ba73",
+    }),
+  ],
+});
